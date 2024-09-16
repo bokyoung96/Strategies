fbea8f342d56adca13e4273e307f846af97e8047
BoKyoungChoi <bokyoung9693@gmail.com>
2024-09-16 21:11:34 +0900

    ADJ: Add IntraTrading1 data

--- IntraTrading1/add_key_vars.py
+++ IntraTrading1/add_key_vars.py
@@ -7,13 +7,14 @@ class AddKeyVars:
         self.intra_data_ = intra_data.copy()
         self.daily_data_ = daily_data.copy()
         self.dividends_ = dividends.copy()
-        
+
         self.rolling_vol = rolling_vol
         self.rolling_move = rolling_move
-        
-        self.intra_data_['day'] = pd.to_datetime(self.intra_data_['caldt']).dt.date
+
+        self.intra_data_['day'] = pd.to_datetime(
+            self.intra_data_['caldt']).dt.date
         self.intra_data_.set_index(['caldt'], inplace=True)
-        
+
         self.intra_data_['move_open'] = np.nan
         self.intra_data_['vwap'] = np.nan
         self.intra_data_['d_vol'] = np.nan
@@ -29,10 +30,11 @@ class AddKeyVars:
     @property
     def d_ret(self):
         return pd.Series(index=self.days, dtype=float)
-    
+
     @staticmethod
     def calculate_vwap(curr_day_data):
-        hlc = (curr_day_data['high'] + curr_day_data['low'] + curr_day_data['close']) / 3
+        hlc = (curr_day_data['high'] +
+               curr_day_data['low'] + curr_day_data['close']) / 3
         vol_hlc = curr_day_data['volume'] * hlc
         cum_vol_hlc = vol_hlc.cumsum()
         cum_vol = curr_day_data['volume'].cumsum()
@@ -58,30 +60,40 @@ class AddKeyVars:
             prev_day_data = daily_groups.get_group(prev_day)
 
             # VWAP
-            self.intra_data_.loc[curr_day_data.index, 'vwap'] = self.calculate_vwap(curr_day_data)
+            self.intra_data_.loc[curr_day_data.index,
+                                 'vwap'] = self.calculate_vwap(curr_day_data)
 
             # MOVE
             open_price = curr_day_data['open'].iloc[0]
-            self.intra_data_.loc[curr_day_data.index, 'move_open'] = self.calculate_move_open(curr_day_data, open_price)
+            self.intra_data_.loc[curr_day_data.index, 'move_open'] = self.calculate_move_open(
+                curr_day_data, open_price)
 
             # VOLATILITY
-            d_ret.loc[curr_day] = curr_day_data['close'].iloc[-1] / prev_day_data['close'].iloc[-1] - 1
+            d_ret.loc[curr_day] = curr_day_data['close'].iloc[-1] / \
+                prev_day_data['close'].iloc[-1] - 1
             if day >= self.rolling_vol:
-                self.intra_data_.loc[curr_day_data.index, 'd_vol'] = self.calculate_volatility(d_ret, curr_day, prev_day, self.rolling_vol)
+                self.intra_data_.loc[curr_day_data.index, 'd_vol'] = self.calculate_volatility(
+                    d_ret, curr_day, prev_day, self.rolling_vol)
 
     def apply_time_metrics(self):
-        self.intra_data_['min_from_open'] = ((self.intra_data_.index - self.intra_data_.index.normalize()) / pd.Timedelta(minutes=1)) - 9 * 60
-        self.intra_data_['min_of_day'] = self.intra_data_['min_from_open'].round().astype(int)
+        self.intra_data_['min_from_open'] = (
+            (self.intra_data_.index - self.intra_data_.index.normalize()) / pd.Timedelta(minutes=1)) - 9 * 60
+        self.intra_data_['min_of_day'] = self.intra_data_[
+            'min_from_open'].round().astype(int)
         minute_groups = self.intra_data_.groupby('min_of_day')
 
         self.intra_data_['move_open_rolling_mean'] = minute_groups['move_open'].transform(
-            lambda x: x.rolling(window=self.rolling_move, min_periods=self.rolling_move - 1).mean()
+            lambda x: x.rolling(window=self.rolling_move,
+                                min_periods=self.rolling_move - 1).mean()
         )
-        self.intra_data_['sigma_open'] = minute_groups['move_open_rolling_mean'].transform(lambda x: x.shift(1))
+        self.intra_data_['sigma_open'] = minute_groups['move_open_rolling_mean'].transform(
+            lambda x: x.shift(1))
 
     def merge_dividends(self):
-        self.dividends_['day'] = pd.to_datetime(self.dividends_['caldt']).dt.date
-        self.intra_data_ = self.intra_data_.merge(self.dividends_[['day', 'dividend']], on='day', how='left')
+        self.dividends_['day'] = pd.to_datetime(
+            self.dividends_['caldt']).dt.date
+        self.intra_data_ = self.intra_data_.merge(
+            self.dividends_[['day', 'dividend']], on='day', how='left')
         self.intra_data_['dividend'] = self.intra_data_['dividend'].fillna(0)
 
     def run(self):
@@ -96,7 +108,7 @@ class AddKeyVars:
 #     daily_data = pd.read_pickle('./data/233740_daily.pkl')
 #     dividends = pd.read_pickle('./data/233740_dividends.pkl')
 
-#     cls = AddKeyVars(intra_data=intra_data, 
-#                      daily_data=daily_data, 
+#     cls = AddKeyVars(intra_data=intra_data,
+#                      daily_data=daily_data,
 #                      dividends=dividends)
 #     result = cls.run()
